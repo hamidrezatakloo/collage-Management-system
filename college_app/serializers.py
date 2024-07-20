3708b191761872cae101ae23c09f6ab3968e3fc6
hamidrezatakloo <takloohamidreza@gmail.com>
2024-07-20 22:24:40 +0330

    :sparkles: feat: define serializers for models

---
+++ college_app/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import User, Course, Enrollment
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
+
+class SemesterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'user_type')
+        extra_kwargs = {
+            'email': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
