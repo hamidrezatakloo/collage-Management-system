33dd0dda009666c9fc69e9eee41fc20bb8f97ad9
hamidrezatakloo <takloohamidreza@gmail.com>
2024-07-20 22:23:55 +0330

    :sparkles: feat: configure admin tab for custom user model

---
+++ college_app/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'user_type', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('user_type',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('user_type',)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
